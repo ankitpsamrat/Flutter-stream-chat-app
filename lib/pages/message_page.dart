7fc329825bed1b8801acc2d2ff37e903d68b3675
ankitpsamrat <ankit.p.samrat@gmail.com>
2022-05-27 18:12:33 +0530

    first cummit

--- lib/pages/message_page.dart
+++ lib/pages/message_page.dart
@@ -1,12 +1,239 @@
+import 'package:chat_app/helpers.dart';
+import 'package:chat_app/models/message_data.dart';
+import 'package:chat_app/models/story_data.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/theme.dart';
+import 'package:chat_app/widgets/avatar.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+// import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('messagepage'),
+    return CustomScrollView(
+      slivers: [
+         SliverToBoxAdapter(
+          child: _Stories(),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(_delegate),
+        )
+      ],
+    );
+  }
+
+  Widget _delegate(BuildContext context, int index) {
+    final Faker faker = Faker();
+    final date = Helpers.randomDate();
+    return _MessageTitle(
+      messageData: MessageData(
+        senderName: faker.person.name(),
+        message: faker.lorem.sentence(),
+        messageDate: date,
+        dateMessage: Jiffy(date).fromNow(),
+        profilePicture: Helpers.randomPictureUrl(),
+      ),
+    );
+  }
+}
+
+class _MessageTitle extends StatelessWidget {
+  const _MessageTitle({
+    Key? key,
+    required this.messageData,
+  }) : super(key: key);
+
+  final MessageData messageData;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(ChatScreen.route(messageData));
+      },
+      child: Container(
+        height: 100,
+        margin:  EdgeInsets.symmetric(horizontal: 8),
+        decoration:  BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey,
+              width: 0.2,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding:  EdgeInsets.all(4.0),
+          child: Row(
+            children: [
+              Padding(
+                padding:  EdgeInsets.all(10.0),
+                child: Avatar.medium(url: messageData.profilePicture),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding:  EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        messageData.senderName,
+                        overflow: TextOverflow.ellipsis,
+                        style:  TextStyle(
+                          letterSpacing: 0.2,
+                          wordSpacing: 1.5,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      child: Text(
+                        messageData.message,
+                        overflow: TextOverflow.ellipsis,
+                        style:  TextStyle(
+                          fontSize: 12,
+                          color: AppColors.textFaded,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:  EdgeInsets.only(right: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                     SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      messageData.dateMessage.toUpperCase(),
+                      style:  TextStyle(
+                        fontSize: 11,
+                        letterSpacing: -0.2,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.textFaded,
+                      ),
+                    ),
+                     SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      width: 18,
+                      height: 18,
+                      decoration:  BoxDecoration(
+                        color: AppColors.secondary,
+                        shape: BoxShape.circle,
+                      ),
+                      child:  Center(
+                        child: Text(
+                          '1',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: AppColors.textLigth,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Stories extends StatelessWidget {
+  const _Stories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      child: SizedBox(
+        height: 134,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+             Padding(
+              padding: EdgeInsets.only(left: 16.0, top: 8, bottom: 16),
+              child: Text(
+                'Stories',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 15,
+                  color: AppColors.textFaded,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  final faker = Faker();
+                  return Padding(
+                    padding:  EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 60,
+                      child: _StoryCard(
+                        storyData: StoryData(
+                          name: faker.person.name(),
+                          url: Helpers.randomPictureUrl(),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  const _StoryCard({
+    Key? key,
+    required this.storyData,
+  }) : super(key: key);
+
+  final StoryData storyData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Avatar.medium(url: storyData.url),
+        Expanded(
+          child: Padding(
+            padding:  EdgeInsets.only(top: 16.0),
+            child: Text(
+              storyData.name,
+              overflow: TextOverflow.ellipsis,
+              style:  TextStyle(
+                fontSize: 11,
+                letterSpacing: 0.3,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,142 +1,3 @@
-// import 'package:chat_app/pages/call_page.dart';
-// import 'package:chat_app/pages/contact_page.dart';
-// import 'package:chat_app/pages/message_page.dart';
-// import 'package:chat_app/pages/notification_page.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-
-// class HomeScreen extends StatelessWidget {
-//   HomeScreen({Key? key}) : super(key: key);
-
-//   final ValueNotifier<int> pageIndex = ValueNotifier(0);
-
-//   final pages = const [
-//     MessagesPage(),
-//     NotificationsPage(),
-//     CallsPage(),
-//     ContactsPage(),
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: ValueListenableBuilder(
-//         valueListenable: pageIndex,
-//         builder: (BuildContext context, int value, _) {
-//           return pages[value];
-//         },
-//       ),
-//       bottomNavigationBar: _BottomNavigationBar(
-//         onItemSelected: (index) {
-//           pageIndex.value = index;
-//         },
-//       ),
-//     );
-//   }
-// }
-
-// class _BottomNavigationBar extends StatelessWidget {
-//   const _BottomNavigationBar({
-//     Key? key,
-//     required this.onItemSelected,
-//   }) : super(key: key);
-
-//   final ValueChanged<int> onItemSelected;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       top: false,
-//       bottom: true,
-//       child: Container(
-//         color: Colors.red,
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceAround,
-//           children: [
-//             _NavigationBarItem(
-//               index: 0,
-//               lable: 'Messages',
-//               icon: CupertinoIcons.bubble_left_bubble_right_fill,
-//               onTap: onItemSelected,
-//             ),
-//             _NavigationBarItem(
-//               index: 1,
-//               lable: 'Notifications',
-//               icon: CupertinoIcons.bell_solid,
-//               onTap: onItemSelected,
-//             ),
-//             _NavigationBarItem(
-//               index: 2,
-//               lable: 'Calls',
-//               icon: CupertinoIcons.phone_fill,
-//               onTap: onItemSelected,
-//             ),
-//             _NavigationBarItem(
-//               index: 3,
-//               lable: 'Contacts',
-//               icon: CupertinoIcons.person_2_fill,
-//               onTap: onItemSelected,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class _NavigationBarItem extends StatelessWidget {
-//   const _NavigationBarItem({
-//     Key? key,
-//     required this.lable,
-//     required this.icon,
-//     required this.index,
-//     required this.onTap,
-//   }) : super(key: key);
-
-//   final int index;
-//   final String lable;
-//   final IconData icon;
-//   final ValueChanged<int> onTap;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: () {
-//         onTap:
-//         (index);
-//       },
-//       child: SizedBox(
-//         height: 70,
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           children: [
-//             Icon(
-//               icon,
-//               size: 20,
-//             ),
-//             SizedBox(
-//               height: 8,
-//             ),
-//             Text(
-//               lable,
-//               style: TextStyle(fontSize: 11),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-//
-
-// import 'package:chatter/pages/calls_page.dart';
-// import 'package:chatter/pages/contacts_page.dart';
-// import 'package:chatter/pages/messages_page.dart';
-// import 'package:chatter/pages/notifications_page.dart';
-// import 'package:chatter/theme.dart';
-// import 'package:chatter/widgets/glowing_action_button.dart';
-// import 'package:chatter/widgets/widgets.dart';
 import 'package:chat_app/helpers.dart';
 import 'package:chat_app/pages/call_page.dart';
 import 'package:chat_app/pages/contact_page.dart';
@@ -144,10 +5,10 @@ import 'package:chat_app/pages/message_page.dart';
 import 'package:chat_app/pages/notification_page.dart';
 import 'package:chat_app/theme.dart';
 import 'package:chat_app/widgets/avatar.dart';
+import 'package:chat_app/widgets/glowing_action_button.dart';
+import 'package:chat_app/widgets/icon_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-// import '../helpers.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -205,7 +66,7 @@ class HomeScreen extends StatelessWidget {
         ),
         actions: [
           Padding(
-            padding: EdgeInsets.only(right: 24.0),
+            padding: EdgeInsets.only(right: 24),
             child: Avatar.small(
               url: Helpers.randomPictureUrl(),
             ),
@@ -253,7 +114,7 @@ class __BottomNavigationBarState extends State<_BottomNavigationBar> {
     return Card(
       color: (brightness == Brightness.light) ? Colors.transparent : null,
       elevation: 0,
-      margin: const EdgeInsets.all(0),
+      margin: EdgeInsets.all(0),
       child: SafeArea(
         top: false,
         bottom: true,
@@ -352,7 +213,9 @@ class _NavigationBarItem extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                       color: AppColors.secondary,
                     )
-                  : TextStyle(fontSize: 11),
+                  : TextStyle(
+                      fontSize: 11,
+                    ),
             ),
           ],
         ),
